dd6a668fbe0cc3254def84142b7d91697845c790
Diego Baes <diegobaesv@gmail.com>
2025-06-04 22:24:02 -0500

    agregado gson

--- app/src/main/java/com/sise/botonpanico/repositories/IncidenciaRepository.java
+++ app/src/main/java/com/sise/botonpanico/repositories/IncidenciaRepository.java
@@ -1,12 +1,15 @@
 package com.sise.botonpanico.repositories;
 
+import com.google.gson.Gson;
 import com.sise.botonpanico.entities.Incidencia;
 import com.sise.botonpanico.shared.Callback;
+import com.sise.botonpanico.shared.Constants;
+import com.sise.botonpanico.shared.HttpUtil;
 
 public class IncidenciaRepository {
 
     public void insertarIncidencia(Incidencia incidencia, Callback callback) {
-
+        HttpUtil.POST(Constants.BASE_URL_API, "incidencias", new Gson().toJson(incidencia));
     }
 
 }
---
+++ app/src/main/java/com/sise/botonpanico/shared/Constants.java
@@ -0,0 +1,5 @@
+package com.sise.botonpanico.shared;
+
+public class Constants {
+    public static String BASE_URL_API = "http://localhost:3000/api/v1";
+}
